b62a24689f2e340a4b31289f9d26f0a499651111
Lander <inflbrazo@elorrieta-errekamari.com>
2019-01-24 11:41:58 +0100

    Branch1

--- src/Kalkulatu.java
+++ src/Kalkulatu.java
@@ -3,6 +3,7 @@ public class Kalkulatu {
 	
 	
 	public int gehiketa (int x, int y) {
+		System.out.println("Branch1");
 		x++;
 		y++;
 		return x+y;
@@ -10,6 +11,7 @@ public class Kalkulatu {
 	}
 	
 		public int kenketa (int x, int y) {
+			y++;
 		return x-y;
 		
 	}
